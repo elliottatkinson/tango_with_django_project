d8b7c14228c3c8b8f80f717d1ad538ade785f4f5
Elliott Atkinson <2769539A@student.gla.ac.uk>
2024-02-08 22:17:35 +0000

    chapter 8

--- rango/views.py
+++ rango/views.py
@@ -28,10 +28,11 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-
-    context_dict = {'boldmessage': 'This tutorial has been put together by Elliott'}
     
-    return render(request, 'rango/about.html', context=context_dict)
+    print(request.method)
+    print(request.user)
+    
+    return render(request, 'rango/about.html', {})
 
 
 
